7abdd2eadd3030d394ebbce38ec9b0c00f2f05f1
Jonathan Searra <jonathan.searra@gmail.com>
2023-12-14 16:26:34 +0100

    Setup base components

--- unsplash/src/app/app.routes.ts
+++ unsplash/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) },
+  { path: 'details/:id', loadChildren: () => import('./details/details.module').then(m => m.DetailsModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
